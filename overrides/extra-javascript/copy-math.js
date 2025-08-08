73a0875102cfd2f969dfdb840e5deaa9bf453a4a
Yash Shah <yashshah00@outlook.com>
2025-08-08 20:02:02 +0100

    Deployed 179dfbb with MkDocs version: 1.6.1

---
+++ overrides/extra-javascript/copy-math.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".typst-math").forEach((el) => {
+    el.addEventListener("click", () => {
+      const typst = el.getAttribute("data-typst");
+      if (typst) {
+        console.log(typst);
+        navigator.clipboard.writeText(typst).then(() => {
+          // Feedback
+          el.classList.add("copied");
+          setTimeout(() => 250);
+          setTimeout(() => el.classList.remove("copied"), 250);
+
+          // Remove and force reflow to retrigger animation
+          el.classList.remove("border-green-500");
+          void el.offsetWidth;
+          el.classList.add("border-green-500");
+
+          // Remove the green border after fade-out delay
+          setTimeout(() => {
+            el.classList.remove("border-green-500");
+          }, 800); // Match the transition duration + a bit of delay
+        });
+      }
+    });
+  });
+});
+
+document.getElementsByClassName("external_links")[0].classList.add("flex-wrap", "flex-gap");
+document.querySelector('img[alt="image"]').classList.add("rounded-xl");
+document.getElementById("search-dialog").parentElement.classList.remove("hidden");
+document.querySelector('input[data-slot="command-input"]').placeholder = "Search everything...";
+document.querySelector('span.text-muted-foreground.hidden.lg\\:inline-flex').textContent = "Search everything...";
+
